b88346bac95f2f77b3a282a843327289f8a71c65
babarismail <babarismail@gmail.com>
2018-02-25 15:05:11 -0800

    Add files via upload

---
+++ dateformat.js
@@ -0,0 +1,10 @@
+;(function() {
+		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994648", "id123-control37995199")};
+		
+	})();
+
+function format(idFrom, idTo) {
+    var x = document.getElementById(idFrom).value;
+    document.getElementById(idTo).value = x;
+    
+}
