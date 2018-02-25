de3903d444564748bd97e18897e3833be96ca7f1
babarismail <babarismail@gmail.com>
2018-02-25 15:55:43 -0800

    Add files via upload

--- dateformat.js
+++ dateformat.js
@@ -1,10 +1,18 @@
 ;(function() {
 		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994648", "id123-control37995199")};
+		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994658", "id123-control37995202")};
+		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994693", "id123-control37995206")};
+		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994718", "id123-control37995204")};
+		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994696", "id123-control37995205")};
+		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994720", "id123-control37995203")};
+		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994756", "id123-control37995223")};
 		
 	})();
 
 function format(idFrom, idTo) {
-    var x = document.getElementById(idFrom).value;
-    document.getElementById(idTo).value = x;
+    var dateToConvert = document.getElementById(idFrom).value;
+    Date dateObj = new Date(dateToConvert);
+    var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    document.getElementById(idTo).value = dateObj.getDate() + " " + months[dateObj.getMonth()] + " " + dateObj.getFullYear();
     
 }
