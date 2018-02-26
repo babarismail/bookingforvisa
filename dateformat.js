3e3bcf0a076ecc145fa4ee858447619b2c1fd4dd
babarismail <babarismail@gmail.com>
2018-02-25 16:27:38 -0800

    Add files via upload

--- dateformat.js
+++ dateformat.js
@@ -1,11 +1,11 @@
 ;(function() {
 		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994648", "id123-control37995199")};
-		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994658", "id123-control37995202")};
-		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994693", "id123-control37995206")};
-		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994718", "id123-control37995204")};
-		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994696", "id123-control37995205")};
-		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994720", "id123-control37995203")};
-		document.getElementById("id123-control37994648").onchange = function() {format("id123-control37994756", "id123-control37995223")};
+		document.getElementById("id123-control37994658").onchange = function() {format("id123-control37994658", "id123-control37995202")};
+		document.getElementById("id123-control37994693").onchange = function() {format("id123-control37994693", "id123-control37995206")};
+		document.getElementById("id123-control37994718").onchange = function() {format("id123-control37994718", "id123-control37995204")};
+		document.getElementById("id123-control37994696").onchange = function() {format("id123-control37994696", "id123-control37995205")};
+		document.getElementById("id123-control37994720").onchange = function() {format("id123-control37994720", "id123-control37995203")};
+		document.getElementById("id123-control37994756").onchange = function() {format("id123-control37994756", "id123-control37995223")};
 		
 	})();
 
