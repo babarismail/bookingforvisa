a0724497b66a9c04e94d36a9d4ede50dc890fbb1
babarismail <babarismail@gmail.com>
2018-05-02 16:59:04 -0700

    Add files via upload

---
+++ orderplaced.js
@@ -0,0 +1,17 @@
+<!-- Google Code for Form Completed Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 814638123;
+var google_conversion_label = "dDpiCLC1noEBEKvIuYQD";
+var google_conversion_value = 15.00;
+var google_conversion_currency = "USD";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/814638123/?value=15.00&amp;currency_code=USD&amp;label=dDpiCLC1noEBEKvIuYQD&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
