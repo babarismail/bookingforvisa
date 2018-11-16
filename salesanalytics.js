81bf5a8698c1b523ec994a24df2d76ee522323b7
babarismail <babarismail@gmail.com>
2018-11-15 18:19:43 -0800

    Add files via upload

---
+++ salesanalytics.js
@@ -0,0 +1,20 @@
+<!-- Global site tag (gtag.js) - Google Ads: 814638123 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-814638123"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-814638123');
+</script>
+
+<!-- Event snippet for Sales conversion page -->
+<script>
+  gtag('event', 'conversion', {
+      'send_to': 'AW-814638123/vjarCJjKvI8BEKvIuYQD',
+      'transaction_id': ''
+  });
+</script>
+
+
+
